d26f4e1d85c7252f8cf298de3e95cdfdc26e3e1f
Lucas Pijl <77995433+cacher300@users.noreply.github.com>
2023-09-18 21:35:39 -0400

    Create linkgrabber.py

---
+++ linkgrabber.py
@@ -0,0 +1,20 @@
+import re
+
+# Specify the file path
+file_path = "links.txt"  # Replace with your file's path
+
+try:
+    # Open the file in read mode ('r')
+    with open(file_path, 'r') as file:
+        # Read the entire file content into a string
+        input_text = file.read()
+
+# Define a regular expression pattern to match links within HTML attributes
+link_pattern = r'href="([^"]+)"'
+
+# Find all matches of the pattern in the input text
+matches = re.findall(link_pattern, input_text)
+
+# Print the detected links
+for match in matches:
+    print(match)
