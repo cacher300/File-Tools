28ed17ea93cbc3f9b16b6403dcf527aa0ff1a02a
Lucas Pijl <77995433+cacher300@users.noreply.github.com>
2023-09-19 00:20:49 -0400

    Update linkgrabber.py

--- linkgrabber.py
+++ linkgrabber.py
@@ -9,12 +9,17 @@ try:
         # Read the entire file content into a string
         input_text = file.read()
 
-# Define a regular expression pattern to match links within HTML attributes
-link_pattern = r'href="([^"]+)"'
+    # Define a regular expression pattern to match links within HTML attributes
+    link_pattern = r'href="([^"]+)"'
 
-# Find all matches of the pattern in the input text
-matches = re.findall(link_pattern, input_text)
+    # Find all matches of the pattern in the input text
+    matches = re.findall(link_pattern, input_text)
 
-# Print the detected links
-for match in matches:
-    print(match)
+    # Print the detected links
+    for match in matches:
+        print(match)
+
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
